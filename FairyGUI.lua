f9940a53401b5afebff33e10281d023f729409b6
lavender <497703565@qq.com>
2020-10-09 15:00:26 +0800

    modif format

--- FairyGUI.lua
+++ FairyGUI.lua
@@ -1,39 +1,39 @@
 -----------------通用-----------------------------
     --  sortingOrder: 设置显示层级：panel.SetSortingOrder(1, true);
---                    UIPanel在屏幕上的显示顺序是由他的sortingOrder属性决定的。sortingOrder越大，显示在越前面（越靠近屏幕）。
---                    对sortingOrder为100的UIPanel按z进行排序，z值越小，显示在越前面。Stage.inst.SortWorldSpacePanelsByZOrder(100);
+    --                UIPanel在屏幕上的显示顺序是由他的sortingOrder属性决定的。sortingOrder越大，显示在越前面（越靠近屏幕）。
+    --                对sortingOrder为100的UIPanel按z进行排序，z值越小，显示在越前面。Stage.inst.SortWorldSpacePanelsByZOrder(100);
 
 -----------------GList-----------------------------
 
---##Properties
-    --	actualHeight:绝对高度=height * scaleY
-    --  actualWidth:绝对宽度=width * scalex
-    --  columnCount：列数
-    --  columnGap: 列距
-    --	Controllers：控制器
-    --  draggable：可拖拽(list.draggable = true/false)
-    --  grayed:变灰 
-    --	numChildren：返回子节点个数
-    --	numItems：设置子对象个数，可用于刷新列表
-    --  onClick
-    --  onClickItem:子对象点击事件
-            -- self:AddListener(self.list.onClickItem,function(context)
-            --         local item = context.data   --这个item就是点击的item 可操作
-            --     )
+    --##Properties
+        --	actualHeight:绝对高度=height * scaleY
+        --  actualWidth:绝对宽度=width * scalex
+        --  columnCount：列数
+        --  columnGap: 列距
+        --	Controllers：控制器
+        --  draggable：可拖拽(list.draggable = true/false)
+        --  grayed:变灰 
+        --	numChildren：返回子节点个数
+        --	numItems：设置子对象个数，可用于刷新列表
+        --  onClick
+        --  onClickItem:子对象点击事件
+                -- self:AddListener(self.list.onClickItem,function(context)
+                --         local item = context.data   --这个item就是点击的item 可操作
+                --     )
 
-    --  onDragStart：拖动开始
-    --  onDragMove：拖动ing
-    --  onDragStart：拖动结束
+        --  onDragStart：拖动开始
+        --  onDragMove：拖动ing
+        --  onDragStart：拖动结束
 
-    --  onTouchBegin
-    --  onTouchMove
-    --  onTouchEnd
+        --  onTouchBegin
+        --  onTouchMove
+        --  onTouchEnd
 
 
 -----------------GObject-----------------------------
 
---##Method
-    --  GObject.TweenMoveX(float endValue，float duration)
-        --param[结束位置，播完动画所需时间]
-    --  GObject.TweenMoveY(float endValue，float duration)
+    --##Method
+        --  GObject.TweenMoveX(float endValue，float duration)
+            --param[结束位置，播完动画所需时间]
+        --  GObject.TweenMoveY(float endValue，float duration)
 
