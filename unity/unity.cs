c37c1a7c088bb1ef829f76dff977ca76a8714dd9
lavender <497703565@qq.com>
2021-09-10 15:58:18 +0800

    feat:新增lua node unity加载 note

--- unity/unity.cs
+++ unity/unity.cs
@@ -179,3 +179,44 @@
     // Unity的工程切换到Android平台后，运行游戏出现shader丢失
 
     // 解决办法：在Unity桌面图标的快捷方式后添加 -force-gles20
+
+    //几种加载方式的区别:
+    // 其实存在3种加载方式：
+
+    // 一是静态引用，建一个public的变量，在Inspector里把prefab拉上去，用的时候instantiate
+
+    // 二是Resource.Load，Load以后instantiate
+
+    // 三是AssetBundle.Load,Load以后instantiate
+
+    // 三种方式有细 节差异，前两种方式，引用对象texture是在instantiate时加载，
+        //而assetBundle.Load会把perfab的全部assets 都加载，instantiate时只是生成Clone。
+        //所以前两种方式，除非你提前加载相关引用对象，否则第一次instantiate时会包含加载引用 assets的操作，导致第一次加载的lag（滞后）。
+
+    // Unity中的内存种类
+    // 实际上Unity游戏使用的内存一共有三种**：程序代码、托管堆（Managed Heap）以及本机堆（Native Heap）**。
+
+    // 程序代码包括了所有的Unity引擎，使用的库，以及你所写的所有的游戏代码。在编译后，得到的运行文件将会被加载到设备中执行，并占用一定内存。
+
+    // 这部分内存实际上是没有办法去“管理”的，它们将在内存中从一开始到最后一直存在。一个空的Unity默认场景，什么代码都不放，在iOS设备上占 用内存应该在17MB左右，而加上一些自己的代码很容易就飙到20MB左右。想要减少这部分内存的使用，能做的就是减少使用的库，稍后再说。
+
+    // 托管堆是被Mono使用的一部分内存。Mono项目一个开源的.net框架的一种实现，对于Unity开发，其实充当了基本类库的角色。
+
+    // 托管堆用来存放类的实例（比如用new生成的列表，实例中的各种声明的变量等）。“托管”的意思是Mono“应该”自动地改变堆的大小来适应你所需要的内存，
+
+    // 并且定时地使用垃圾回收（Garbage Collect）来释放已经不需要的内存。关键在于，有时候你会忘记清除对已经不需要再使用的内存的引用，
+
+    // 从而导致Mono认为这块内存一直有用，而无法回收。
+
+    // 最后，本机堆是Unity引擎进行申请和操作的地方，比如贴图，音效，关卡数据等。Unity使用了自己的一套内存管理机制来使这块内存具有和托管堆类似的功能。
+
+    // 基本理念是，如果在这个关卡里需要某个资源，那么在需要时就加载，之后在没有任何引用时进行卸载。听起来很美好也和托管堆一样，
+
+    // 但是由于Unity有一套自动加载和卸载资源的机制，让两者变得差别很大。自动加载资源可以为开发者省不少事儿，
+
+    // 但是同时也意味着开发者失去了手动管理所有加载资源的权力，这非常容易导致大量的内存占用（贴图什么的你懂的），
+
+    // 也是Unity给人留下“吃内存”印象的罪魁祸首。
+
+    //来源：
+    //https://blog.csdn.net/m0_46184795/article/details/108266234?utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7EOPENSEARCH%7Edefault-12.control&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7EOPENSEARCH%7Edefault-12.control
