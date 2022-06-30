483c613644c92b442a28ce4001a596f79b5c1c54
lavender <497703565@qq.com>
2022-06-30 14:10:46 +0800

    feat : add list note !

--- python/file_op/copyFile.py
+++ python/file_op/copyFile.py
@@ -5,7 +5,7 @@ import shutil
 from telnetlib import theNULL 
 
 src  = "C:/Users/xiaojiang\Desktop/pycopy/file1/test.txt"
-src1  = "C:/Users/xiaojiang\Desktop/pycopy/test.txt"
+src1  = "C:/Users/xiaojiang\Desktop/test.txt"
 des = "C:/Users/xiaojiang/Desktop/pycopy/file2"
 rootfile = "C:/Users/xiaojiang/Desktop/pycopy"
 
@@ -37,7 +37,7 @@ rootfile = "C:/Users/xiaojiang/Desktop/pycopy"
         # if os.path.isfile(full_file_name):
                 
 
- 
+ #指定文件夹下搜索某个文件
 def scaner_file (url, fileName):
         file  = os.listdir(url)
         for f in file:
@@ -48,38 +48,43 @@ def scaner_file (url, fileName):
 
                         # 如果是文件，则以绝度路径的方式输出
                 elif path.isdir(real_url):
-                #如果是目录，则是地柜调研自定义函数 scaner_file (url)进行多次
+                #如果是目录，则是地柜调研自定义函数 scaner_file (url)进行多次【递归】
                         scaner_file(real_url)
                 else:
- 
                         print("其他情况")
                         pass
 
-
-def update_file (url, fileurl, fileName):
-        file  = os.listdir(url)
+#指定文件下，更新指定文件名的所有文件
+#targetUrl : 需要更新文件夹
+#updateFileAbsUrl : 更新文件绝对路径带文件名
+#updateFileName ： 更新文件名
+def update_file (targetUrl, updateFileAbsUrl, updateFileName):
+        file  = os.listdir(targetUrl)
+        print(">>>>>>>>>>>>>>> 当前遍历文件夹 ：",targetUrl,"<<<<<<<<<<<<<<<")
         for f in file:
-                real_url = path.join (url , f)
+                real_url = path.join (targetUrl , f)
                 if path.isfile(real_url):
                         # 如果是文件，则以绝度路径的方式输出
-                        print("文件的绝对路径路径 = ",path.abspath(real_url))
-
-                        print("更新文件名称 = ",fileName)
+                        print("更新文件名称 = ",updateFileName)
                         print("当前文件名 = ",f)
-                        if f==fileName:
+                        if f==updateFileName:
+                                #先删除后复制
                                 if os.path.exists(real_url):
                                         os.remove(real_url)
-                                print("当前路径 = ",url)
-                                print("更新文件绝对路径 = ",fileurl)
-                                if os.path.exists(fileurl):
-                                        shutil.copy(fileurl, url)
+                                        print("删除文件的绝对路径路径 = ",path.abspath(real_url))
+                                print("当前路径 = ",targetUrl)
+                                print("更新文件绝对路径 = ",updateFileAbsUrl)
+                                if os.path.exists(updateFileAbsUrl):
+                                        shutil.copy(updateFileAbsUrl, targetUrl)
+                                        print("更新文件suc: ",f)
                 elif path.isdir(real_url):
-                #如果是目录，则是地柜调研自定义函数 scaner_file (url)进行多次
-                        update_file(real_url,fileurl, fileName)
+                #如果是目录，则是地柜调研自定义函数 update_file (targetUrl)进行多次【递归】
+                        update_file(real_url,updateFileAbsUrl, updateFileName)
                 else:
- 
                         print("其他情况")
                         pass
 
 # scaner_file(rootfile)
 update_file(rootfile, src1, "test.txt")
+
+
