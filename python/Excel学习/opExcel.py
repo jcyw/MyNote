214423096446e1794bac20c52a09d0ddef8d82dd
lavender <497703565@qq.com>
2023-02-09 15:27:35 +0800

    feat:新增python Excel 笔记

--- python/Excel学习/opExcel.py
+++ python/Excel学习/opExcel.py
@@ -10,7 +10,12 @@
 
 # 1.载入Excel       注意load_workbook只能打开已经存在的Excel，不能创建新的工作簿
 from openpyxl import load_workbook
-workbook = load_workbook('Excel学习.xlsx') 
+from openpyxl.styles import Font
+from openpyxl.styles import Alignment
+from openpyxl.styles import Side, Border
+from openpyxl.styles import PatternFill , GradientFill
+fileName = 'C:/Users/xiaojiang/Desktop/大杂烩/gitP/MyNote/python/Excel学习/Excel学习.xlsx'
+workbook = load_workbook(fileName)
 print(workbook.sheetnames)
 # 2.根据名称获取工作表
 # 如果只有一张工作表也可以用：
@@ -46,3 +51,73 @@ cells = sheet[5:6]
 # 当然，上面的三种方法都是获取一堆表格，现在要输出每一个表格的值就需要遍历：
 # for cellchild in cells:
 #     print(cellchild.value)
+
+# 指定返回取值
+# for row in sheet.iter_rows(min_row = 1, max_row = 3,min_col = 2, max_col = 5):
+#     print(row)
+#     for cell in row:
+#         print(cell.value)
+
+# 7.读取所有的行
+# for row in sheet.rows:
+#     print(row)
+
+# 写入单元格
+cell = sheet['A1']
+cell.value = 'test'
+
+# 写入一行或者多行数据
+# data1 = ['小明', 25 , "五年级"]
+# sheet.append(data1)
+# data2 = ['小江', 25 , "六年级"]
+# sheet.append(data2)
+data = [
+    ['小明', 25 , "五年级"],
+    ['小江', 25 , "六年级"]
+]
+# for i in data:
+#     sheet.append(i)
+
+# 将公式写入单元格保存
+sheet['K11'] = '=AVERAGE(K1:K10)' 
+
+# 设置样式
+# Font(name名称 , size大小, bold粗体, italic斜体, color颜色)
+font = Font(name='微软雅黑', size=5,bold=True, italic=True, color='FF000000')
+cell.font = font
+# 设置对齐方式
+# Alignment(horizontal=水平对齐, vertical=垂直对齐, text_rotation=字体倾斜度, wrap_text=自动换行)
+# 水平对齐:distributed, justify, center, left, fill, centerContinuous, right, general
+# 垂直对齐:bottom, distributed, justify, center, top
+alignment = Alignment(horizontal='center', vertical='center', text_rotation=45, wrap_text=True)
+cell.alignment = alignment
+# 设置边框样式
+# 边线样式:double, mediumDashDotDot, slantDashDot, dashDotDot, dotted, hair, mediumDashed, dashed, dashDot, thin, mediumDashDot, medium, thick
+side = Side(style='thin', color='FF000000')
+border = Border(left=side , right=side , top=side , bottom=side)
+cell.border = border
+#设置单元格填充格式
+cell = sheet['A2']
+pattern_fill = PatternFill(patternType='solid',fgColor='99ccff')
+cell.fill = pattern_fill
+cell2 = sheet['A3']
+gradient_fill = GradientFill(stop=('FFFFFF','99ccff','000000'))
+cell2.fill = gradient_fill
+# 设置行高和列宽
+sheet.row_dimensions[1].height = 50 
+sheet.column_dimensions['C'].width = 20 
+
+# 6. 单元格合并与取消
+
+# 合并
+sheet.merge_cells('A1:B2') 
+sheet.merge_cells(start_row=1, start_column=3, 
+                  end_row=2, end_column=4)
+
+# 取消合并
+sheet.unmerge_cells('A1:B2') 
+sheet.unmerge_cells(start_row=1, start_column=3, 
+                    end_row=2, end_column=4)
+
+# 保存Excel
+workbook.save(fileName)
