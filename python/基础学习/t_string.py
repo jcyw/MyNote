da7a7c7eea494f6eaf2a096707769761fda1c3a1
lavender <497703565@qq.com>
2023-03-09 18:52:33 +0800

    feat:新增字符串笔记

---
+++ python/基础学习/t_string.py
@@ -0,0 +1,9 @@
+# 获取python字符串的后4个字符：
+string = "example"
+last_four = string[-4:]
+print(last_four)
+
+# Python 去除字符串后面 4 个字符
+string = "hello world!" # 声明一个字符串变量
+result = string[:-4] # 利用字符串截取去除最后4个字符
+print(result) # 输出结果为 "hello world"
